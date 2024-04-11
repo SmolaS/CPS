31971d05d6239169990477b2a6c9d3b3a61d7400
Szymon Smoła <116088570+SmolaS@users.noreply.github.com>
2024-04-11 18:59:37 +0200

    Update LAB5.jl

--- LAB5.jl
+++ LAB5.jl
@@ -1,8 +1,18 @@
 
-## Problem 5.1
+## Problem 5.1 Zaimplementuj funkcję quantize: ℝ^𝑁 → 𝐹, która zwróci funkcje 𝑓𝐿 ∈ 𝐹, gdzie
+## 𝐹 to zbiór funkcji ℝ → ℝ. Funkcja 𝑓𝐿(𝑥) powinna zwracać dla wartości 𝑥 ∈ ℝ poziom kwantyzacji
+## 𝑙′ ∈ 𝐿 poprzez rozwiązanie problemu:
+## 𝑙′ = arg min(𝑙∈𝐿)‖𝑥 − 𝑙‖2,
+## gdzie 𝐿 = [𝑙1, 𝑙2, …, 𝑙𝑁 ]^𝑇 ∈ ℝ^𝑁 zawiera poziomy kwantyzacji które spełniają warunek 𝑙𝑖 ≠ 𝑙𝑗 dla 𝑖 ≠ 𝑗.
 
-## Problem 5.2
 
-## Problem 5.3
+## Problem 5.2 Zaimplementuj funkcję SQNR: ℕ^+ → 𝑅, która obliczy teoretyczny stosunek mocy sygnału do mocy szumu kwantyzacji
+## wyrażony w decybelach dla idealnego 𝑁 ∈ ℕ^+ bitowego przetwornika analogowo cyfrowego.
 
-## Problem 5.4
+
+## Problem 5.3 Zaimplementuj funkcję SNR: ℝ^+ × ℝ^+ → 𝑅, która obliczy stosunek mocy sygnału do mocy szumu 
+## wyrażony w decybelach.
+
+
+## Problem 5.4 Zbadaj zależność między teoretycznymi i rzeczywistymi wartościami SQNR dla róznych sygnałów.
+ 
