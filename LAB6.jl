20be9610802adea1f1518b6ef46311ad86f1fe0e
Szymon Smoła <116088570+SmolaS@users.noreply.github.com>
2024-04-11 18:32:59 +0200

    Update LAB6.jl

--- LAB6.jl
+++ LAB6.jl
@@ -3,8 +3,6 @@
 
 ## Problem 6.2
 
-## Problem 6.3
-
 ## Problem 6.4
 
 ## Problem 6.5
@@ -12,17 +10,3 @@
 ## Problem 6.6
 
 ## Problem 6.7
-
-## Problem 6.8
-
-## Problem 6.9
-
-## Problem 6.10
-
-## Problem 6.11
-
-## Problem 6.12
-
-## Problem 6.13
-
-## Problem 6.14
