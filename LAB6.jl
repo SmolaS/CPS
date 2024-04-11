00c6562624613d92b7bc931a67696976d173a26e
Szymon Smoła <116088570+SmolaS@users.noreply.github.com>
2024-04-11 18:54:44 +0200

    Update LAB6.jl

--- LAB6.jl
+++ LAB6.jl
@@ -1,12 +1,33 @@
 
-## Problem 6.1
+## Problem 6.1 Oblicz dwu punktową dyskretną transformacje Fouriera (2-DFT) sygnału 𝑥 ∈ ℝ^2
+## 𝑥 = (20, 5)
 
-## Problem 6.2
+## Problem 6.2  Oblicz cztero punktową dyskretną transformacje Fouriera (4-DFT) sygnału 𝑥 ∈ℝ^4
+## 𝑥 = (3, 2, 5, 1)
 
-## Problem 6.4
 
-## Problem 6.5
+## Problem 6.4 : Zaimplementuj funkcję dft: ℂ^𝑁 → ℂ^𝑁 dla 𝑁 ∈ ℕ^+, 
+## która przekształci dyskretny sygnał 𝑥 ∈ ℂ^𝑁 z domeny czasu do odpowiadającego mu dyskretnego sygnału
+## 𝑋 ∈ ℂ^𝑁 w domenie częstotliwości (dwustronne dyskretne widmo częstotliwościowe).
+## Do obliczenia tego przekształcenia wykorzystaj dyskretną transformację 
+## Fouriera oraz algorytm naiwny (tzn. bezpośrednio ze wzoru).
 
-## Problem 6.6
+## Problem 6.5 Zaimplementuj funkcję idft: ℂ^𝑁 → ℂ^𝑁 dla 𝑁 ∈ ℕ^+, 
+## która przekształci dyskretny sygnał 𝑋 ∈ ℂ^𝑁 z domeny częstotliwości 
+## (dwustronne dyskretne widmo częstotliwościowe) do odpowiadającego mu dyskretnego sygnału 𝑥 ∈ ℂ
+## 𝑁 w domenie czasu. Do obliczenia tego przekształcenia wykorzystaj odwrotną dyskretną transformację
+## Fouriera oraz algorytm naiwny (tzn. bezpośrednio ze wzoru).
 
-## Problem 6.7
+## Problem 6.6 Zaimplementuj funkcję rdft: ℝ^𝑁 → ℂ^⌊𝑁/2]+1 dla 𝑁 ∈ ℕ^+, która przekształci
+## dyskretny sygnał 𝑥 ∈ ℝ^𝑁 z domeny czasu do odpowiadającego mu dyskretnego sygnału 𝑋 ∈ ℂ^⌊𝑁/2]+1
+## w domenie częstotliwości (jednostronne dyskretne widmo częstotliwościowe). 
+## Do obliczenia tego przekształcenia wykorzystaj dyskretną transformację Fouriera oraz 
+## algorytm naiwny (tzn. bezpośrednio ze wzoru).
+
+## Problem 6.7 Zaimplementuj funkcję irdft: ℂ^⌊𝑁/2]+1 → R^𝑁 dla 𝑁 ∈ ℕ^+, która przekształci
+## dyskretny sygnał 𝑋 ∈ ℂ^⌊𝑁/2]+1 z domeny częstotliwości (jednostronne dyskretne widmo częstotliwościowe) 
+## do odpowiadającego mu dyskretnego sygnału 𝑥 ∈ ℝ^𝑁 w domenie czasu.
+## Do obliczenia tego przekształcenia wykorzystaj odwrotną dyskretną transformację Fouriera oraz 
+## naiwny algorytm (tzn. bezpośrednio ze wzoru).
+
+
