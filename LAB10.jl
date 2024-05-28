ef17ce2631448aa6045e7193c29d0a3358dc0f2b
Szymon Smoła <116088570+SmolaS@users.noreply.github.com>
2024-05-28 16:53:29 +0200

    lab10

---
+++ LAB10.jl
@@ -0,0 +1,4 @@
+## Problem 10.1: Zaimplementuj funkcję resample: ℝ^𝑁 × ℕ × ℕ → ℝ^𝐾, która oszacuje
+## pierwsze 𝐾 próbki dyskretnego sygnału 𝑔[𝑛] = 𝑓(𝑛/𝑓2) na podstawie 𝑁 próbek dyskretnego
+## sygnału 𝑥[𝑛] = 𝑓(𝑛/𝑓1), gdzie 𝑓2 =(M/N)*𝑓1, 𝑀, 𝑁 ∈ ℕ^+.
+
